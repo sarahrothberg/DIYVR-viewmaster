582685b15a47ccd07007432e23183bdec046ae43
sarahrothberg <sarahrothberg@gmail.com>
2016-10-04 12:18:49 -0400

    init for viewmaster

---
+++ Assets/YiTingsCoolScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YiTingsCoolScript : MonoBehaviour {
+
+	public GameObject coolVariable;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("hi this is the start loop"); 
+	}
+}
---
+++ Assets/texturesArray.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class texturesArray : MonoBehaviour {
+
+	public Texture[] myTextures; 
+	private int currentNum; 
+
+	// Use this for initialization
+	void Start () {
+	
+		GetComponent<Renderer> ().material.mainTexture = myTextures [0]; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			GetComponent<Renderer> ().material.mainTexture = myTextures [currentNum]; 
+			currentNum++; 
+			if (currentNum == myTextures.Length) {
+				currentNum = 0; 
+			}
+		}
+	}
+}
